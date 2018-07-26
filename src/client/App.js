2fa74abc563a0e06f9b098078a34f39e0c265621
pavellevap2 <paveltextfree@gmail.com>
2018-07-26 19:26:47 +0300

    Make first version

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  color: red;
+`;
+
+class App extends Component {
+  render() {
+    return <Title>Hello World</Title>;
+  }
+}
+
+export default App;
