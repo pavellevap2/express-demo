98664b4559545fe48533b2a7758ab1e103993a3f
pavellevap2 <paveltextfree@gmail.com>
2018-07-30 14:33:26 +0300

    Start passport

--- server/index.js
+++ server/index.js
@@ -2,14 +2,11 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const FS = require("fs");
+const process = require("process");
+const HTTP = require("http");
 const app = express();
 
-const HTTP = {
-  STATUS_CODES: {
-    404: "Data is not defined",
-    500: "Interval server error"
-  }
-};
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.use(
   bodyParser.urlencoded({
@@ -17,33 +14,12 @@ app.use(
   })
 );
 
-app.use("/public", express.static(path.join(__dirname, "../public")));
-
-app.get("/api/users", (req, res, next) => {
-  FS.readFile(path.join(__dirname, "./db/users.json"), "utf-8", (err, data) => {
-    try {
-      if (err) {
-        throw { status: 500 };
-      } else if (!data) {
-        throw { status: 404 };
-      }
-
-      res.status(200).send(data);
-    } catch (e) {
-      return next(e);
-    }
-  });
-});
-
-function errorHandler(err, req, res, next) {
-  res.status(err.status);
-  res.send({ message: HTTP.STATUS_CODES[err.status] });
-}
-
-app.use(errorHandler);
-
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`listens on port ${PORT} -> http://localhost:${PORT}`);
+  console.log(
+    `listens on port ${PORT} -> http://localhost:${PORT}, on process -> ${
+      process.pid
+    }`
+  );
 });
